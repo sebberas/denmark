78a9c150c6ec102a1a3a8f93c4a13ec04f0b79a9
sebberas <sebberas@gmail.com>
2025-11-13 14:33:27 +0100

    Added some more functions

--- backend/src/datafordeler/cvr.rs
+++ backend/src/datafordeler/cvr.rs
@@ -1,44 +1,21 @@
-use std::env;
-use std::io::{Cursor};
-
 use ::serde_json::{Value as JsonValue};
-use ::anyhow::anyhow;
 use ::reqwest::Client;
-use ::reqwest::StatusCode;
-use ::chrono::{DateTime, Utc};
 
-const BASE_URL: &str = "https://api.datafordeler.dk/FileDownloads/GetFile";
-
-
-#[tracing::instrument(skip(client))]
-pub async fn download_file(client: &Client, filename: &str) -> anyhow::Result<JsonValue> {
-    let url = format!("{BASE_URL}?Filename={filename}&apikey={}", env::var("DATAFORDELER_API_KEY")?);
-    let now = Utc::now();
-    
-    
-    let request = client.get(url);
-
-    let response = request.send().await?;
-
-    match response.status() {
-        StatusCode::OK => {
-            // TODO: Check if zip?
-            let body = response.bytes().await?;
-            let mut archive = zip::ZipArchive::new(Cursor::new(body))?;
-            let entry = archive.by_index(0)?;
-            let contents = serde_json::from_reader::<_, JsonValue>(entry)?;            
-            Ok(contents)
-        }
-        status => {
-            let text = response.text().await;
-            Err(anyhow!("failed to download file {status}\n{text:?}"))
-        }
-    }
-}
+use super::download_file;
 
 #[tracing::instrument(skip(client))]
 pub async fn download_branche_list(client: &Client) -> anyhow::Result<JsonValue> {
     download_file(client, "CVR_V1_Branche_TotalDownload_json_Current_193.zip").await
+}
+
+#[tracing::instrument(skip(client))]
+pub async fn download_navn_list(client: &Client) -> anyhow::Result<JsonValue> {
+    download_file(client, "CVR_V1_Navn_TotalDownload_json_Current_193.zip").await
+}
+
+#[tracing::instrument(skip(client))]
+pub async fn download_enhed_list(client: &Client) -> anyhow::Result<JsonValue> {
+    download_file(client, "CVR_V1_CVREnhed_TotalDownload_json_Bitemporal_193.zip").await
 }
 
 #[cfg(test)]
